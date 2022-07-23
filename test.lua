c5e7012cd7fa50842be5b20d447d52638c588029
aikidouser <t36085520@icloud.com>
2022-07-23 20:52:37 +0800

    Rewrite

--- test.lua
+++ test.lua
@@ -1,28 +1,35 @@
-vim.cmd([[
-  set autoread
-  set cmdheight=1
-  set cursorcolumn
-  set cursorline
-  set encoding=utf-8
-  set expandtab
-  set ignorecase
-  set linebreak
-  set number
-  set shiftwidth=2
-  set updatetime=200
-  set wildmenu
-]])
+vim.opt.autoread = true
+vim.opt.cmdheight = 1
+vim.opt.cursorcolumn = true
+vim.opt.cursorline = true
+vim.opt.encoding = "utf-8"
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.linebreak = true
+vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.updatetime = 200
+vim.opt.wildmenu = true
 
--- Status
+-- Status Line
 -- Init to empty
-vim.cmd("set statusline=")
+vim.opt.statusline = ""
 -- %F: Full path of the file
 -- %M: If the file was modified and unsave, there will be a "+"
 -- %Y: File type
 -- %R: There will be the flag if the file is ReadOnly
-vim.cmd("set statusline+=\ %F\ %M\ %Y\ %R")
+vim.opt.statusline += "\ %F\ %M\ %Y\ %R"
 -- Seperate left and right side
-vim.cmd("set statusline+=%=")
+vim.opt.statusline += "%="
 -- %p%%: percentage
-vim.cmd("set statusline+=\ row:\ %l\ col:\ %c\ \ %p%%")
+vim.opt.statusline += "\ row:\ %l\ col:\ %c\ \ %p%%"
 
+-- colorscheme
+vim.opt.colorscheme = dracula
+
+-- Plugin Settings
+
+-- rainbow 
+vim.g.rainbow_active = 1
+
+-- Load Settings
