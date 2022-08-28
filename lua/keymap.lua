a5357321355dc446e6c8e762ca1d4946dd4f3f83
aikidouser <t36085520@icloud.com>
2022-08-28 17:09:42 +0800

    Set keymap of Telescope oldfile and livegrep

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 vim.keymap.set("i", "jl", "<ESC>", {noremap = true})
 
 -- Buffer
-vim.keymap.set("n", "<leader>b", ":ls<CR>:b ", {noremap = true})
+vim.keymap.set("n", "<leader>b", ":Telescope buffers<CR>", {noremap = true})
 
 -- Copy and Past
 vim.keymap.set({'n', 'v'}, '<leader>c', '"+y', {noremap = true})
@@ -31,4 +31,6 @@ vim.keymap.set("n", "<leader>ts", ":sp<CR><C-w>j:terminal<CR>", {noremap = true}
 -- Telescope
 vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>", {noremap = true})
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>lg", ":Telescope live_grep<CR>", {noremap = true})
 
