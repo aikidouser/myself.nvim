bbe41e047dbb09876874ed3eddc97fd7dfbb3447
aikidouser <t36085520@icloud.com>
2023-06-06 21:19:54 +0800

    Add keymap: session select

--- lua/user/core/keymap.lua
+++ lua/user/core/keymap.lua
@@ -1,9 +1,9 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 vim.keymap.set('i', '<C-\\>', '<ESC>', { noremap = true })
 
 -- Buffer
-vim.keymap.set("n", "<leader>b", ":Telescope buffers<CR>", { noremap = true })
+vim.keymap.set('n', '<leader>b', ':Telescope buffers<CR>', { noremap = true })
 
 -- Copy and Past
 vim.keymap.set({ 'n', 'v' }, '<leader>c', '"+y', { noremap = true })
@@ -14,7 +14,7 @@ vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true })
 
 -- Source
 vim.keymap.set('n', '<leader>e', ':e %<CR>', { noremap = true })
-vim.keymap.set('n', '<leader>s', ':source %<CR>', { noremap = true })
+-- vim.keymap.set('n', '<leader>s', ':source %<CR>', { noremap = true })
 
 -- Tab
 vim.keymap.set('n', 'gn', ':tabnew<CR>', { noremap = true })
@@ -23,15 +23,18 @@ vim.keymap.set('n', 'g2', ':tablast<CR>', { noremap = true })
 vim.keymap.set('n', 'g1', ':tabfirst<CR>', { noremap = true })
 
 -- terminal
--- vim.keymap.set("t", "<leader>t<ESC>", "<C-\\><C-n>", { noremap = true })
-vim.keymap.set("n", "<leader>tf", ":FTermToggle<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>td", ":FTermExit<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>tg", ":FTermTig<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>tv", ":vs<CR><C-w>l:terminal<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>ts", ":sp<CR><C-w>j:terminal<CR>", { noremap = true })
+-- vim.keymap.set('t', '<leader>t<ESC>', '<C-\\><C-n>', { noremap = true })
+vim.keymap.set('n', '<leader>tf', ':FTermToggle<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>td', ':FTermExit<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>tg', ':FTermTig<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>tv', ':vs<CR><C-w>l:terminal<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>ts', ':sp<CR><C-w>j:terminal<CR>', { noremap = true })
 
 -- Telescope
-vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>lg", ":Telescope live_grep<CR>", { noremap = true })
+vim.keymap.set('n', '<leader>fb', ':Telescope file_browser<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>fo', ':Telescope oldfiles<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>lg', ':Telescope live_grep<CR>', { noremap = true })
+
+-- Session
+vim.keymap.set('n', '<leader>s', ':lua MiniSessions.select()<CR>', { noremap = true })
